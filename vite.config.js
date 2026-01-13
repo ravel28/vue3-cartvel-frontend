dab608e20ed817699e40a003ee752c7aa56d1bd8
ravel28 <ravelinno9@gmail.com>
2026-01-13 20:37:21 +0700

    [update] fixing bug after prod

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
+// jika menggunakan ES Module (import/export)
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path"; // ← wajib import path
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
